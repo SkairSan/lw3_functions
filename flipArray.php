fc4533e249323c85cf3478bcf97b47a1aac9b9d0
SkairSan <vlad.muckob@mail.ru>
2026-03-25 22:49:04 +0000

    flipArray.php. Удалил ?>, убрал встроенную функцию

--- flipArray.php
+++ flipArray.php
@@ -1,6 +1,13 @@
 <?php
 
 function flipArray(array $arr): array {
-    return array_flip($arr);
+    $result = [];
+    
+    foreach ($arr as $key => $value) {
+        if (is_string($value) || is_int($value)) {
+            $result[$value] = $key;
+        }
+    }
+     
+    return $result;
 }
-?>
