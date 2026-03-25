fdfdaaba34cfc2c9542d0f89f242d5bd2b0d0b56
SkairSan <vlad.muckob@mail.ru>
2026-03-25 23:22:50 +0000

    areAnagrams.php. Убрал ?>, расписал функции

--- areAnagrams.php
+++ areAnagrams.php
@@ -1,15 +1,48 @@
 <?php
 
 function areAnagrams(string $a, string $b): bool {
-    $cleanA = str_replace(' ', '', strtolower($a));
-    $cleanB = str_replace(' ', '', strtolower($b));
+    $lowerA = strtolower($a);
+    $lowerB = strtolower($b);
     
-    $arrA = str_split($cleanA);
-    $arrB = str_split($cleanB);
+    if (strlen($lowerA) !== strlen($lowerB)) {
+        return false;
+    }
+
+    $len = strlen($lowerA);
     
-    sort($arrA);
-    sort($arrB);
+    $arrA = [];
+    $arrB = [];
     
-    return $arrA === $arrB;
+    for ($i = 0; $i < $len; $i++) {
+        $arrA[] = $lowerA[$i];
+        $arrB[] = $lowerB[$i];
+    }
+    
+    for ($i = 0; $i < $len - 1; $i++) {
+        for ($j = 0; $j < $len - $i - 1; $j++) {
+            if ($arrA[$j] > $arrA[$j + 1]) {
+                $temp = $arrA[$j];
+                $arrA[$j] = $arrA[$j + 1];
+                $arrA[$j + 1] = $temp;
+            }
+            if ($arrB[$j] > $arrB[$j + 1]) {
+                $temp = $arrB[$j];
+                $arrB[$j] = $arrB[$j + 1];
+                $arrB[$j + 1] = $temp;
+            }
+        }
+    }
+    
+    for ($i = 0; $i < $len; $i++) {
+        if ($arrA[$i] !== $arrB[$i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
-?>
+
+
+
+    
+}
